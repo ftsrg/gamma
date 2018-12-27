3778dbf3a26f3da53d7dfa149fbb28dde3db1a88
Bence Graics <grbeni@gmail.com>
2018-12-27 14:59:54 +0100

    Type check implementation started

--- plugins/hu.bme.mit.gamma.constraint.language/src/hu/bme/mit/gamma/constraint/language/validation/ConstraintLanguageValidator.java
+++ plugins/hu.bme.mit.gamma.constraint.language/src/hu/bme/mit/gamma/constraint/language/validation/ConstraintLanguageValidator.java
@@ -10,6 +10,39 @@
  ********************************************************************************/
 package hu.bme.mit.gamma.constraint.language.validation;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.xtext.validation.Check;
+
+import hu.bme.mit.gamma.constraint.language.validation.ExpressionTypeDeterminator.ExpressionType;
+import hu.bme.mit.gamma.constraint.model.AddExpression;
+import hu.bme.mit.gamma.constraint.model.ArithmeticExpression;
+import hu.bme.mit.gamma.constraint.model.BinaryExpression;
+import hu.bme.mit.gamma.constraint.model.BooleanLiteralExpression;
+import hu.bme.mit.gamma.constraint.model.BooleanTypeDefinition;
+import hu.bme.mit.gamma.constraint.model.ConstraintModelPackage;
+import hu.bme.mit.gamma.constraint.model.DecimalLiteralExpression;
+import hu.bme.mit.gamma.constraint.model.DivExpression;
+import hu.bme.mit.gamma.constraint.model.DivideExpression;
+import hu.bme.mit.gamma.constraint.model.Expression;
+import hu.bme.mit.gamma.constraint.model.IntegerLiteralExpression;
+import hu.bme.mit.gamma.constraint.model.IntegerTypeDefinition;
+import hu.bme.mit.gamma.constraint.model.ModExpression;
+import hu.bme.mit.gamma.constraint.model.MultiaryExpression;
+import hu.bme.mit.gamma.constraint.model.MultiplyExpression;
+import hu.bme.mit.gamma.constraint.model.NaturalTypeDefinition;
+import hu.bme.mit.gamma.constraint.model.RationalLiteralExpression;
+import hu.bme.mit.gamma.constraint.model.RealTypeDefinition;
+import hu.bme.mit.gamma.constraint.model.ReferenceExpression;
+import hu.bme.mit.gamma.constraint.model.SubtractExpression;
+import hu.bme.mit.gamma.constraint.model.Type;
+import hu.bme.mit.gamma.constraint.model.UnaryExpression;
+import hu.bme.mit.gamma.constraint.model.UnaryMinusExpression;
+import hu.bme.mit.gamma.constraint.model.UnaryPlusExpression;
+import hu.bme.mit.gamma.constraint.model.VariableDeclaration;
 
 /**
  * This class contains custom validation rules. 
@@ -18,15 +51,285 @@ package hu.bme.mit.gamma.constraint.language.validation;
  */
 public class ConstraintLanguageValidator extends AbstractConstraintLanguageValidator {
 	
-//	public static final INVALID_NAME = 'invalidName'
-//
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital",
-//					ConstraintLanguagePackage.Literals.GREETING__NAME,
-//					INVALID_NAME);
-//		}
+	protected ExpressionTypeDeterminator typeDeterminator = new ExpressionTypeDeterminator();
+	
+	@Check
+	public void checkArithmeticExpression(ArithmeticExpression expression) {
+		try {
+			if (expression instanceof UnaryExpression) {
+				// + or -
+				UnaryExpression unaryExpression = (UnaryExpression) expression;
+				if (!typeDeterminator.isNumber(unaryExpression.getOperand())) {
+					error("The operand of the unary arithemtic operator is evaluated as a non-number value.",
+							ConstraintModelPackage.Literals.UNARY_EXPRESSION__OPERAND);
+				}
+			}
+			else if (expression instanceof BinaryExpression) {
+				// - or / or mod or div
+				BinaryExpression binaryExpression = (BinaryExpression) expression;
+				if (!typeDeterminator.isNumber(binaryExpression.getLeftOperand())) {
+					error("The left operand of the binary arithemtic operator is evaluated as a non-number value.",
+							ConstraintModelPackage.Literals.BINARY_EXPRESSION__LEFT_OPERAND);
+				}
+				if (!typeDeterminator.isNumber(binaryExpression.getLeftOperand())) {
+					error("The right operand of the binary arithemtic operator is evaluated as a non-number value.",
+							ConstraintModelPackage.Literals.BINARY_EXPRESSION__RIGHT_OPERAND);
+				}
+			}
+			else if (expression instanceof MultiaryExpression) {
+				// + or *
+				MultiaryExpression multiaryExpression = (MultiaryExpression) expression;
+				for (int i = 0; i < multiaryExpression.getOperands().size(); ++i) {
+					Expression operand = multiaryExpression.getOperands().get(i);
+					if (!typeDeterminator.isNumber(operand)) {
+						error("The operand of the multiary arithemtic operator is evaluated as a non-number value.",
+								ConstraintModelPackage.Literals.MULTIARY_EXPRESSION__OPERANDS, i);
+					}
+				}
+			}
+		} catch (Exception exception) {
+			// There is a type error on a lower level, no need to display the error message on this level too
+		}
+	}
+	
+	@Check
+	public void checkVariableDeclaration(VariableDeclaration variableDeclaration) {
+		if (variableDeclaration.getExpression() == null) {
+			return;
+		}
+		final Type variableDeclarationType = variableDeclaration.getType();
+		final ExpressionType initialExpressionType = typeDeterminator.getType(variableDeclaration.getExpression());
+		if (!typeDeterminator.equals(variableDeclarationType, initialExpressionType)) {
+			error("The type of the variable declaration and the initial expression are not the same.",
+					ConstraintModelPackage.Literals.DEFINABLE_DECLARATION__EXPRESSION);
+		}
+	}
+	
+}
+
+class ExpressionTypeDeterminator {
+	
+	enum ExpressionType { BOOLEAN, NATURAL, INTEGER, REAL }
+	
+	/**
+	 * Collector of extension methods.
+	 */
+	public ExpressionType getType(Expression expression) {
+		if (expression instanceof BooleanLiteralExpression) {
+			return getType((BooleanLiteralExpression) expression);
+		}
+		if (expression instanceof IntegerLiteralExpression) {
+			return getType((IntegerLiteralExpression) expression);
+		}
+		if (expression instanceof RationalLiteralExpression) {
+			return getType((RationalLiteralExpression) expression);
+		}
+		if (expression instanceof DecimalLiteralExpression) {
+			return getType((DecimalLiteralExpression) expression);
+		}
+		if (expression instanceof ReferenceExpression) {
+			return getType((ReferenceExpression) expression);
+		}
+		if (expression instanceof UnaryPlusExpression) {
+			return getArithmeticUnaryType((UnaryPlusExpression) expression);
+		}
+		if (expression instanceof UnaryMinusExpression) {
+			return getArithmeticUnaryType((UnaryMinusExpression) expression);
+		}
+		if (expression instanceof SubtractExpression) {
+			return getArithmeticBinaryType((SubtractExpression) expression);
+		}
+		if (expression instanceof DivideExpression) {
+			return getArithmeticBinaryType((DivideExpression) expression);
+		}
+		if (expression instanceof ModExpression) {
+			return getArithmeticBinaryIntegerType((ModExpression) expression);
+		}
+		if (expression instanceof DivExpression) {
+			return getArithmeticBinaryIntegerType((DivExpression) expression);
+		}
+		if (expression instanceof AddExpression) {
+			return getArithmeticMultiaryType((AddExpression) expression);
+		}
+		if (expression instanceof MultiplyExpression) {
+			return getArithmeticMultiaryType((MultiplyExpression) expression);
+		}
+		throw new IllegalArgumentException("Not known expression: " + expression);
+	}
+	
+	// Extension methods
+	
+	// Literals
+	
+	private ExpressionType getType(BooleanLiteralExpression expression) {
+		return ExpressionType.BOOLEAN;
+	}
+	
+	private ExpressionType getType(IntegerLiteralExpression expression) {
+		return ExpressionType.INTEGER;
+	}
+	
+	private ExpressionType getType(RationalLiteralExpression expression) {
+		return ExpressionType.REAL;
+	}
+	
+	private ExpressionType getType(DecimalLiteralExpression expression) {
+		return ExpressionType.REAL;
+	}
+	
+	// References
+	private ExpressionType getType(ReferenceExpression expression) {
+		Type declarationType = expression.getDeclaration().getType();
+		return transform(declarationType);
+	}
+	
+	// Predicates
+	
+	// Booleans
+	
+	// Arithmetics
+	
+	private ExpressionType getArithmeticType(Collection<ExpressionType> collection) {
+		// Wrong types, not suitable for arithmetic operations
+		if (collection.stream().anyMatch(it -> isNumber(it))) {
+			throw new IllegalArgumentException("Type not suitable for arithmetic operations: " + collection);
+		}
+		// All types are numbers
+		if (collection.stream().anyMatch(it -> it == ExpressionType.REAL)) {
+			return ExpressionType.REAL;
+		}
+		if (collection.stream().anyMatch(it -> it == ExpressionType.INTEGER)) {
+			return ExpressionType.INTEGER;
+		}
+		return ExpressionType.NATURAL;
+	}
+	
+	// Unary
+	
+	/**
+	 * Unary plus and minus.
+	 */
+	private <T extends ArithmeticExpression & UnaryExpression> ExpressionType getArithmeticUnaryType(T expression) {
+		ExpressionType type = getType(expression.getOperand());
+		if (isNumber(type)) {
+			return type;
+		}
+		throw new IllegalArgumentException("Type not suitable type for expression: " + type + System.lineSeparator() + expression);
+	}
+	
+	// Binary
+	
+	/**
+	 * Subtract and divide.
+	 */
+	private <T extends ArithmeticExpression & BinaryExpression> ExpressionType getArithmeticBinaryType(T expression) {
+		List<ExpressionType> types = new ArrayList<ExpressionType>();
+		types.add(getType(expression.getLeftOperand()));
+		types.add(getType(expression.getRightOperand()));		
+		return getArithmeticType(types);
+	}
+	
+//	private ExpressionType getType(SubtractExpression expression) {
+//		return getArithmeticBinaryType(expression);
 //	}
+//	
+//	private ExpressionType getType(DivideExpression expression) {
+//		return getArithmeticBinaryType(expression);
+//	}
+	
+	/**
+	 * Modulo and div.
+	 */
+	private <T extends ArithmeticExpression & BinaryExpression> ExpressionType getArithmeticBinaryIntegerType(T expression) {
+		ExpressionType type = getArithmeticBinaryType(expression);
+		if (type == ExpressionType.INTEGER || type ==  ExpressionType.NATURAL) {
+			return type;
+		}
+		throw new IllegalArgumentException("Type not suitable type for expression: " + type + System.lineSeparator() + expression);
+	}
+	
+//	private ExpressionType getType(ModExpression expression) {
+//		ExpressionType type = getArithmeticBinaryType(expression);
+//		if (type == ExpressionType.NATURAL || type ==  ExpressionType.INTEGER) {
+//			return type;
+//		}
+//		throw new IllegalArgumentException("Type not suitable for modulo: " + type);
+//	}
+//	
+//	private ExpressionType getType(DivExpression expression) {
+//		ExpressionType type = getArithmeticBinaryType(expression);
+//		if (type == ExpressionType.NATURAL || type ==  ExpressionType.INTEGER) {
+//			return type;
+//		}
+//		throw new IllegalArgumentException("Type not suitable for div: " + type);
+//	}
+	
+	// Multiary
+	
+	/**
+	 * Add and multiply.
+	 */
+	private <T extends ArithmeticExpression & MultiaryExpression> ExpressionType getArithmeticMultiaryType(T expression) {
+		Collection<ExpressionType> types = expression.getOperands().stream()
+				.map(it -> getType(it)).collect(Collectors.toSet());
+		return getArithmeticType(types);
+	}
+	
+//	private ExpressionType getType(AddExpression expression) {
+//		return getArithmeticMultiaryType(expression);
+//	}
+//	
+//	private ExpressionType getType(MultiplyExpression expression) {
+//		return getArithmeticMultiaryType(expression);
+//	}
+	
+	// Easy determination of boolean and number types
+	
+	private boolean isBoolean(ExpressionType type) {
+		return type == ExpressionType.BOOLEAN;
+	}
+	
+	public boolean isBoolean(Expression	expression) {
+		return isBoolean(getType(expression));
+	}
+	
+	private boolean isNumber(ExpressionType type) {
+		return type == ExpressionType.INTEGER ||
+				type == ExpressionType.REAL || 
+				type == ExpressionType.NATURAL;
+	}
+	
+	public boolean isNumber(Expression expression) {
+		return isNumber(getType(expression));
+	}
+	
+	// Transform type
+	
+	public ExpressionType transform(Type type) {
+		if (type instanceof BooleanTypeDefinition) {
+			return ExpressionType.BOOLEAN;
+		}
+		if (type instanceof IntegerTypeDefinition) {
+			return ExpressionType.INTEGER;
+		}
+		if (type instanceof RealTypeDefinition) {
+			return ExpressionType.REAL;
+		}
+		if (type instanceof NaturalTypeDefinition) {
+			return ExpressionType.NATURAL;
+		}
+		throw new IllegalArgumentException("Not known type: " + type);
+	}
+	
+	// Type equal
+	
+	public boolean equals(Type type, ExpressionType expressionType) {
+		return type instanceof BooleanTypeDefinition && expressionType == ExpressionType.BOOLEAN ||
+				type instanceof IntegerTypeDefinition && expressionType == ExpressionType.INTEGER ||
+				type instanceof RealTypeDefinition && expressionType == ExpressionType.REAL || 
+				type instanceof NaturalTypeDefinition && expressionType == ExpressionType.NATURAL; 
+				
+	}
+	
 	
 }
