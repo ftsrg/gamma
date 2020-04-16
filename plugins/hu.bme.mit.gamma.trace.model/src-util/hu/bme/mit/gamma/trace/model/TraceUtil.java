8764009fc2aa92aa43b97f75f9be77042fda1a55
Bence Graics <grbeni@gmail.com>
2020-04-16 13:17:04 +0200

    Extend Trace util with step removal

--- plugins/hu.bme.mit.gamma.trace.model/src-util/hu/bme/mit/gamma/trace/model/TraceUtil.java
+++ plugins/hu.bme.mit.gamma.trace.model/src-util/hu/bme/mit/gamma/trace/model/TraceUtil.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.gamma.trace.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
@@ -14,11 +15,72 @@ public class TraceUtil {
 	public void removeCoveredExecutionTraces(List<ExecutionTrace> traces) {
 		for (int i = 0; i < traces.size() - 1; ++i) {
 			ExecutionTrace lhs = traces.get(i);
-			for (int j = i + 1; j < traces.size(); ++j) {
+			boolean isLhsDeleted = false;
+			for (int j = i + 1; j < traces.size() && !isLhsDeleted; ++j) {
 				ExecutionTrace rhs = traces.get(j);
+				if (isCovered(rhs, lhs)) {
+					traces.remove(j);
+					--j;
+				}
+				else if (isCovered(lhs, rhs)) {
+					// Else is important, as it is possible that both cover the other one
+					isLhsDeleted = true;
+					traces.remove(i);
+					--i;
+				}
 			}
 		}
 	}
+	
+	public void removeCoveredSteps(ExecutionTrace trace) {
+		List<List<Step>> separateTracesByReset = identifySeparateTracesByReset(trace);
+		removeCoveredStepLists(separateTracesByReset);
+	}
+	
+	public List<List<Step>> identifySeparateTracesByReset(ExecutionTrace trace) {
+		List<List<Step>> stepsList = new ArrayList<List<Step>>();
+		List<Step> actualSteps = null;
+		for (Step step : trace.getSteps()) {
+			if (step.getActions().stream().anyMatch(it -> it instanceof Reset)) {
+				if (actualSteps != null) {
+					stepsList.add(actualSteps);
+				}
+				actualSteps = new ArrayList<Step>();
+			}
+			actualSteps.add(step);
+		}
+		return stepsList;
+	}
+	
+	public void removeCoveredStepLists(List<List<Step>> traces) {
+		for (int i = 0; i < traces.size() - 1; ++i) {
+			List<Step> lhs = traces.get(i);
+			boolean isLhsDeleted = false;
+			for (int j = i + 1; j < traces.size() && !isLhsDeleted; ++j) {
+				List<Step> rhs = traces.get(j);
+				if (isCovered(rhs, lhs)) {
+					traces.remove(j);
+					removeFromContainment(rhs);
+					--j;
+				}
+				else if (isCovered(lhs, rhs)) {
+					// Else is important, as it is possible that both cover the other one
+					isLhsDeleted = true;
+					traces.remove(i);
+					removeFromContainment(lhs);
+					--i;
+				}
+			}
+		}
+	}
+	
+	public void removeFromContainment(List<? extends EObject> steps) {
+		for (EObject step : steps) {
+			EObject container = step.eContainer();
+			container.eContents().remove(step);
+		}
+	}
+
 	
 	public boolean isCovered(ExecutionTrace covered, ExecutionTrace covering) {
 		return isCovered(covered.getSteps(), covering.getSteps());
