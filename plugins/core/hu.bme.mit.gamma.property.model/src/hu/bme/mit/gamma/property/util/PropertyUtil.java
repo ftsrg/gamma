aca2fd7c6c7a5d1c4a5b8e7c0f29255e4334cfd6
Bence Graics <grbeni@gmail.com>
2020-07-15 23:47:23 +0200

    Extend property util

--- plugins/core/hu.bme.mit.gamma.property.model/src/hu/bme/mit/gamma/property/util/PropertyUtil.java
+++ plugins/core/hu.bme.mit.gamma.property.model/src/hu/bme/mit/gamma/property/util/PropertyUtil.java
@@ -10,10 +10,54 @@
  ********************************************************************************/
 package hu.bme.mit.gamma.property.util;
 
-public class PropertyUtil {
+import hu.bme.mit.gamma.expression.model.Expression;
+import hu.bme.mit.gamma.expression.util.ExpressionUtil;
+import hu.bme.mit.gamma.property.model.AtomicFormula;
+import hu.bme.mit.gamma.property.model.PathQuantifier;
+import hu.bme.mit.gamma.property.model.PropertyModelFactory;
+import hu.bme.mit.gamma.property.model.QuantifiedFormula;
+import hu.bme.mit.gamma.property.model.StateFormula;
+import hu.bme.mit.gamma.property.model.UnaryOperandPathFormula;
+import hu.bme.mit.gamma.property.model.UnaryPathOperator;
+
+public class PropertyUtil extends ExpressionUtil {
 	// Singleton
 	public static final PropertyUtil INSTANCE = new PropertyUtil();
 	protected PropertyUtil() {}
 	//
+	protected PropertyModelFactory factory = PropertyModelFactory.eINSTANCE;
+	
+	public StateFormula createSimpleCTLFormula(PathQuantifier pathQuantifier,
+			UnaryPathOperator unaryPathOperator, Expression expression) {
+		QuantifiedFormula quantifiedFormula = factory.createQuantifiedFormula();
+		quantifiedFormula.setQuantifier(pathQuantifier);
+		UnaryOperandPathFormula pathFormula = factory.createUnaryOperandPathFormula();
+		pathFormula.setOperator(unaryPathOperator);
+		quantifiedFormula.setFormula(pathFormula);
+		AtomicFormula atomicFormula = factory.createAtomicFormula();
+		atomicFormula.setExpression(expression);
+		pathFormula.setOperand(atomicFormula);
+		return quantifiedFormula;
+	}
+	
+	public StateFormula createEF(Expression expression) {
+		return createSimpleCTLFormula(PathQuantifier.EXISTS, UnaryPathOperator.FUTURE, expression);
+	}
+	
+	public StateFormula createEG(Expression expression) {
+		return createSimpleCTLFormula(PathQuantifier.EXISTS, UnaryPathOperator.GLOBAL, expression);
+	}
+	
+	public StateFormula createAF(Expression expression) {
+		return createSimpleCTLFormula(PathQuantifier.FORALL, UnaryPathOperator.FUTURE, expression);
+	}
+	
+	public StateFormula createAG(Expression expression) {
+		return createSimpleCTLFormula(PathQuantifier.FORALL, UnaryPathOperator.GLOBAL, expression);
+	}
+	
+	public StateFormula createLeadsTo(Expression lhs, Expression rhs) {
+		return null;
+	}
 	
 }
