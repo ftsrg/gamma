ftsrg/gamma

commit 5dfb81532d2d8272e1618ca54786acbe6f34fa56

Add missing variable reference in PS script

plugins/core/hu.bme.mit.gamma.uppaal.verification/src/hu/bme/mit/gamma/uppaal/verification/UppaalVerifier.xtend
@@ -30,7 +30,7 @@ class UppaalVerifier extends AbstractVerifier {
 		val actualUppaalQuery = uppaalQueryFile.loadString
 		try {
 			// verifyta -t0 -T TestOneComponent.xml asd.q 
-			val command = "verifyta " + parameters + " " + uppaalFile.canonicalPath.escapePath + " " + uppaalQueryFile.canonicalPath.escapePath
+			val command = '''verifyta  + «parameters» «uppaalFile.canonicalPath.escapePath» «uppaalQueryFile.canonicalPath.escapePath»'''
 			// Executing the command
 			logger.log(Level.INFO, "Executing command: " + command)
 			process =  Runtime.getRuntime().exec(command)

plugins/xsts/hu.bme.mit.gamma.theta.verification/src/hu/bme/mit/gamma/theta/verification/ThetaVerifier.xtend
@@ -64,7 +64,7 @@ class ThetaVerifier extends AbstractVerifier {
 			// java -jar %THETA_XSTS_CLI_PATH% --model trafficlight.xsts --property red_green.prop
 			val traceFile = new File(modelFile.traceFile)
 			traceFile.delete // So no invalid/old cex is parsed if this actual process does not generate one 
-			val command = "java -jar " + jar.escapePath + " " + parameters + " --model " + modelFile.canonicalPath.escapePath + " --property " + queryFile.canonicalPath.escapePath + " --cex " + traceFile.canonicalPath.escapePath
+			val command = '''java -jar «jar.escapePath» «parameters» --model «modelFile.canonicalPath.escapePath» --property «queryFile.canonicalPath.escapePath» --cex «traceFile.canonicalPath.escapePath»'''
 			// Executing the command
 			logger.log(Level.INFO, "Executing command: " + command)
 			process = Runtime.getRuntime().exec(command)

plugins/xsts/theta-bin/Get-Theta.ps1
@@ -15,11 +15,11 @@ function Download {
 }
 
 Write-Output "Downloading Theta binary"
-Download "https://github.com/ftsrg/theta/releases/download/v2.4.0/theta-xsts-cli.jar" "$currentPath\theta-xsts-cli.jar"
+Download "https://github.com/ftsrg/theta/releases/download/$thetaVersion/theta-xsts-cli.jar" "$currentPath\theta-xsts-cli.jar"
 
 Write-Output "Downloading Z3 solver binaries"
-Download "https://github.com/ftsrg/theta/raw/v2.4.0/lib/libz3.dll" "$currentPath\libz3.dll"
-Download "https://github.com/ftsrg/theta/raw/v2.4.0/lib/libz3java.dll" "$currentPath\libz3java.dll"
+Download "https://github.com/ftsrg/theta/raw/$thetaVersion/lib/libz3.dll" "$currentPath\libz3.dll"
+Download "https://github.com/ftsrg/theta/raw/$thetaVersion/lib/libz3java.dll" "$currentPath\libz3java.dll"
 
 Write-Output "Downloading MSVC++ binaries"
 $zipFilePath = "$currentPath\$z3version.zip"

commit f9a86e087c0a93278c8aec78b2e4f94aa35b662c

Fix typo introduced in 5dfb81532d

plugins/core/hu.bme.mit.gamma.uppaal.verification/src/hu/bme/mit/gamma/uppaal/verification/UppaalVerifier.xtend
@@ -30,7 +30,7 @@ class UppaalVerifier extends AbstractVerifier {
 		val actualUppaalQuery = uppaalQueryFile.loadString
 		try {
 			// verifyta -t0 -T TestOneComponent.xml asd.q 
-			val command = '''verifyta  + «parameters» «uppaalFile.canonicalPath.escapePath» «uppaalQueryFile.canonicalPath.escapePath»'''
+			val command = '''verifyta Â«parametersÂ» Â«uppaalFile.canonicalPath.escapePathÂ» Â«uppaalQueryFile.canonicalPath.escapePathÂ»'''
 			// Executing the command
 			logger.log(Level.INFO, "Executing command: " + command)
 			process =  Runtime.getRuntime().exec(command)