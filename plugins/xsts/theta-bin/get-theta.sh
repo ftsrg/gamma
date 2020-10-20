f25efd75d27dec5cd3e02cf43055f0d990158a29
Akos Hajdu <hajdua@mit.bme.hu>
2020-10-20 23:47:07 +0200

    Install package required by Z3

--- plugins/xsts/theta-bin/get-theta.sh
+++ plugins/xsts/theta-bin/get-theta.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 THETA_VERSION="v2.5.0"
 
+apt-get install libgomp1 -y
+
 wget "https://github.com/ftsrg/theta/releases/download/$THETA_VERSION/theta-xsts-cli.jar" -O ./theta-xsts-cli.jar
 wget "https://github.com/ftsrg/theta/raw/$THETA_VERSION/lib/libz3.so" -O ./libz3.so
 wget "https://github.com/ftsrg/theta/raw/$THETA_VERSION/lib/libz3java.so" -O ./libz3java.so
