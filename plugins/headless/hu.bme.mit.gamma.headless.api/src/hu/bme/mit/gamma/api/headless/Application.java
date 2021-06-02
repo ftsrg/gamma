93ea4b08def45cd07cc73978484fc53de0dbd1c5
Csanád Csuvarszki <csuvi98@hotmail.hu>
2021-06-02 17:06:57 +0200

    Organized imports in Application.java.

--- plugins/headless/hu.bme.mit.gamma.headless.api/src/hu/bme/mit/gamma/api/headless/Application.java
+++ plugins/headless/hu.bme.mit.gamma.headless.api/src/hu/bme/mit/gamma/api/headless/Application.java
@@ -1,49 +1,11 @@
 package hu.bme.mit.gamma.api.headless;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.xtext.resource.XtextResourceSet;
-
-import hu.bme.mit.gamma.action.language.ActionLanguageStandaloneSetup;
-import hu.bme.mit.gamma.ui.GammaApi;
-import hu.bme.mit.gamma.ui.GammaApi.ResourceSetCreator;
-import hu.bme.mit.gamma.expression.language.ExpressionLanguageStandaloneSetup;
-import hu.bme.mit.gamma.genmodel.language.GenModelStandaloneSetup;
-import hu.bme.mit.gamma.property.language.PropertyLanguageStandaloneSetup;
-import hu.bme.mit.gamma.statechart.language.StatechartLanguageStandaloneSetup;
-import hu.bme.mit.gamma.statechart.language.StatechartLanguageStandaloneSetupGenerated;
-import hu.bme.mit.gamma.trace.language.TraceLanguageStandaloneSetup;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.inject.Injector;
-
-import org.apache.commons.io.FileUtils;
 
 public class Application implements IApplication {
 
