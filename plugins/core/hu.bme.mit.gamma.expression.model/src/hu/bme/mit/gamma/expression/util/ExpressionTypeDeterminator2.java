78e0cd73188882da2906c2b1b9f04e7dba278f0a
Bence Graics <grbeni@gmail.com>
2022-05-04 21:48:20 +0200

    Add null checks in the type determinator

--- plugins/core/hu.bme.mit.gamma.expression.model/src/hu/bme/mit/gamma/expression/util/ExpressionTypeDeterminator2.java
+++ plugins/core/hu.bme.mit.gamma.expression.model/src/hu/bme/mit/gamma/expression/util/ExpressionTypeDeterminator2.java
@@ -372,7 +372,7 @@ public class ExpressionTypeDeterminator2 {
 	
 	public boolean isNumber(Expression expression) {
 		try {
-			return isNumber(getType(expression));
+			return expression != null && isNumber(getType(expression));
 		} catch (IllegalArgumentException e) {
 			return false; // e.g., if getType(expression) throws an exception
 		}
@@ -381,18 +381,18 @@ public class ExpressionTypeDeterminator2 {
 	// Type is boolean
 	
 	public boolean isBoolean(Expression expression) {
-		return getTypeDefinition(expression) instanceof BooleanTypeDefinition;
+		return expression != null && getTypeDefinition(expression) instanceof BooleanTypeDefinition;
 	}
 	
 	// Type is integer
 	
 	public boolean isInteger(Expression expression) {
-		return getTypeDefinition(expression) instanceof IntegerTypeDefinition;
+		return expression != null && getTypeDefinition(expression) instanceof IntegerTypeDefinition;
 	}
 	
 	public boolean isInteger(Type type) {
-		return ExpressionModelDerivedFeatures.getTypeDefinition(type) instanceof IntegerTypeDefinition;
-		//return getAliaslessTypeTree(type) instanceof IntegerTypeDefinition;
+		return type != null &&
+				ExpressionModelDerivedFeatures.getTypeDefinition(type) instanceof IntegerTypeDefinition;
 	}
 	
 	// Type pretty printer
