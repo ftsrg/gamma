3d257cce83a6e6cd94938d80846946ca38f3706b
Bence Graics <grbeni@gmail.com>
2022-07-09 00:28:33 +0200

    Fix formatting

--- plugins/core/hu.bme.mit.gamma.action.language/src/hu/bme/mit/gamma/action/language/ActionLanguageRuntimeModule.java
+++ plugins/core/hu.bme.mit.gamma.action.language/src/hu/bme/mit/gamma/action/language/ActionLanguageRuntimeModule.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2018 Contributors to the Gamma project
+ * Copyright (c) 2018-2022 Contributors to the Gamma project
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,8 +10,18 @@
  ********************************************************************************/
 package hu.bme.mit.gamma.action.language;
 
+import org.eclipse.xtext.formatting.IFormatter;
+
+import hu.bme.mit.gamma.action.language.formatting.ActionLanguageFormatter;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class ActionLanguageRuntimeModule extends AbstractActionLanguageRuntimeModule {
+	
+	@Override
+	public Class<? extends IFormatter> bindIFormatter() {
+		return ActionLanguageFormatter.class;
+	}
+	
 }
