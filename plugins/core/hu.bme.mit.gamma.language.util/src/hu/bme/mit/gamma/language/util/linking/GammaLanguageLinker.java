4e6a368afe0ab3bb1c6f7df8e18feca294653998
grbeni <grbeni@gmail.com>
2023-12-21 18:56:04 +0100

    Improve invalid path handling in Gamma model linking

--- plugins/core/hu.bme.mit.gamma.language.util/src/hu/bme/mit/gamma/language/util/linking/GammaLanguageLinker.java
+++ plugins/core/hu.bme.mit.gamma.language.util/src/hu/bme/mit/gamma/language/util/linking/GammaLanguageLinker.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2018-2020 Contributors to the Gamma project
+ * Copyright (c) 2018-2023 Contributors to the Gamma project
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,12 +10,16 @@
  ********************************************************************************/
 package hu.bme.mit.gamma.language.util.linking;
 
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.eclipse.core.internal.resources.ResourceException;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -29,80 +33,87 @@ import com.google.inject.Inject;
 
 public abstract class GammaLanguageLinker extends DefaultLinkingService {
 
-    @Inject IValueConverterService valueConverterService;
-    
-    public List<EObject> getLinkedObjects(EObject context, EReference ref, INode node) {
-    	Map<Class<? extends EObject>, Collection<EReference>> context2 = getContext();
-    	for (Entry<Class<? extends EObject>, Collection<EReference>> entry : context2.entrySet()) {
+	@Inject IValueConverterService valueConverterService;
+	
+	public List<EObject> getLinkedObjects(EObject context, EReference ref, INode node) {
+		Map<Class<? extends EObject>, Collection<EReference>> context2 = getContext();
+		for (Entry<Class<? extends EObject>, Collection<EReference>> entry : context2.entrySet()) {
 			Class<? extends EObject> clazz = entry.getKey();
 			Collection<EReference> references = entry.getValue();
 			if (clazz.isInstance(context) && references.contains(ref)) {
 				try {
-		    		String path = valueConverterService.toValue(node.getText(),
-		    				getLinkingHelper().getRuleNameFrom(node.getGrammarElement()), node).toString().replaceAll("\\s","");
-		    		Resource rootResource = context.eResource();
-		    		ResourceSet resourceSet = rootResource.getResourceSet();
-		    		// Adding the gcd extension, if needed
-		    		String finalPath = addExtensionIfNeeded(path);
-		    		if (!isCorrectPath(finalPath)) {
-		    			// Path of the importer model
-		    			String rootResourceUri = rootResource.getURI().toString();
-		    			StringBuilder pathBuilder = new StringBuilder(finalPath);
-		    			// If the path starts with a '/', we delete it
-		    			if (pathBuilder.charAt(0) == '/') {
-		    				pathBuilder.deleteCharAt(0);
-		    			}
-		    			String[] splittedRootResourceUri = rootResourceUri.split("/");
-		    			int originalCharacterIndex = 0;
-		    			for (int i = 0; i < splittedRootResourceUri.length && !isCorrectPath(pathBuilder.toString()); ++i) {
-		    				// Trying prepending the folders one by one
-		    				String prepension = splittedRootResourceUri[i] + "/";
-		    				pathBuilder.insert(originalCharacterIndex, prepension);
-		    				originalCharacterIndex += prepension.length();
-		    			}
-		    			// Finished
-		    			finalPath = pathBuilder.toString();
-		    		}
-		    		URI uri = URI.createURI(finalPath);
-		    		Resource importedResource = resourceSet.getResource(uri, true);
-		    		// return importedResource.getContents(); would result in an Xtext exception
-		    		// if there is more than one root element in the resource
-		    		EObject importedPackage = importedResource.getContents().get(0);
-		    		return List.of(importedPackage);
+					String path = valueConverterService.toValue(node.getText(),
+							getLinkingHelper().getRuleNameFrom(node.getGrammarElement()), node).toString().replaceAll("\\s","");
+					Resource rootResource = context.eResource();
+					ResourceSet resourceSet = rootResource.getResourceSet();
+					// Adding the gcd extension, if needed
+					String finalPath = addExtensionIfNeeded(path);
+					if (!isCorrectPath(finalPath)) {
+						// Path of the importer model
+						String rootResourceUri = rootResource.getURI().toString();
+						StringBuilder pathBuilder = new StringBuilder(finalPath);
+						// If the path starts with a '/', we delete it
+						if (pathBuilder.charAt(0) == '/') {
+							pathBuilder.deleteCharAt(0);
+						}
+						String[] splittedRootResourceUri = rootResourceUri.split("/");
+						int originalCharacterIndex = 0;
+						for (int i = 0; i < splittedRootResourceUri.length && !isCorrectPath(pathBuilder.toString()); ++i) {
+							// Trying prepending the folders one by one
+							String prepension = splittedRootResourceUri[i] + "/";
+							pathBuilder.insert(originalCharacterIndex, prepension);
+							originalCharacterIndex += prepension.length();
+						}
+						// Finished
+						finalPath = pathBuilder.toString();
+					}
+					URI uri = URI.createURI(finalPath);
+					Resource importedResource = resourceSet.getResource(uri, true);
+					// return importedResource.getContents(); would result in an Xtext exception
+					// if there is more than one root element in the resource
+					EObject importedPackage = importedResource.getContents().get(0);
+					return List.of(importedPackage);
 				} catch (Exception e) {
 					// Trivial case: most of the time (during typing) the uri is not correct, thus the loading cannot be done
 				}
 			}
 		}
-    	return super.getLinkedObjects(context, ref, node);
-    }
-    
-    public abstract Map<Class<? extends EObject>, Collection<EReference>> getContext();
-    
-    private boolean isCorrectPath(String path) {
-    	ResourceSet resourceSet = new ResourceSetImpl();
+		return super.getLinkedObjects(context, ref, node);
+	}
+	
+	public abstract Map<Class<? extends EObject>, Collection<EReference>> getContext();
+	
+	private boolean isCorrectPath(String path) {
+		ResourceSet resourceSet = new ResourceSetImpl();
 		URI uri = URI.createURI(path);
 		try {
-	    	resourceSet.getResource(uri, true);
-	    	resourceSet.getResources().stream().forEach(it -> it.unload());
-	    	resourceSet.getResources().clear();
-	    	resourceSet = null;
-	    	return true;
-		} catch (Exception e) {
-			// Resource cannot be loaded due to invalid path
-			return false;
+			resourceSet.getResource(uri, true);
+			List<Resource> resources = resourceSet.getResources();
+			resources.stream().forEach(it -> it.unload());
+			resources.clear();
+			resourceSet = null;
+			return true;
+		} catch (WrappedException e) {
+			Throwable cause = e.getCause();
+			if (cause instanceof FileNotFoundException || 
+					cause instanceof MalformedURLException ||
+					cause instanceof ResourceException) {
+				// Resource cannot be loaded due to invalid path
+				return false;
+			}
+			throw e;
 		}
-    }
-    
-    // Could be abstract, too
-    private String addExtensionIfNeeded(String path) {
-    	String[] splittedPath = path.split("/");
-    	String fileName = splittedPath[splittedPath.length - 1];
-    	String[] splittedFileName = fileName.split("\\.");
-    	if (splittedFileName.length == 1) {
-    		return path + ".gcd";
-    	}
-    	return path;
-    }
+	}
+	
+	// Could be abstract, too
+	private String addExtensionIfNeeded(String path) {
+		String[] splittedPath = path.split("/");
+		String fileName = splittedPath[splittedPath.length - 1];
+		String[] splittedFileName = fileName.split("\\.");
+		if (splittedFileName.length == 1) {
+			return path + ".gcd";
+		}
+		return path;
+	}
 	
 }
