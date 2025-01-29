a6b168e2a53418445e182110f154bc3fd46ab38e
grbeni <grbeni@gmail.com>
2025-01-29 20:42:05 +0100

    Extend statechart serialization

--- plugins/core/hu.bme.mit.gamma.statechart.language.ui/src/hu/bme/mit/gamma/statechart/language/ui/serializer/StatechartLanguageSerializer.java
+++ plugins/core/hu.bme.mit.gamma.statechart.language.ui/src/hu/bme/mit/gamma/statechart/language/ui/serializer/StatechartLanguageSerializer.java
@@ -19,14 +19,23 @@ import org.eclipse.emf.ecore.EObject;
 import com.google.inject.Injector;
 
 import hu.bme.mit.gamma.language.util.serialization.GammaLanguageSerializer;
+import hu.bme.mit.gamma.statechart.language.StatechartLanguageStandaloneSetupGenerated;
 import hu.bme.mit.gamma.statechart.language.ui.internal.LanguageActivator;
 
 public class StatechartLanguageSerializer {
 
 	public void serialize(EObject rootElem, String parentFolder, String fileName) throws IOException {
 		// This is how an injected object can be retrieved
-		Injector injector = LanguageActivator.getInstance()
-				.getInjector(LanguageActivator.HU_BME_MIT_GAMMA_STATECHART_LANGUAGE_STATECHARTLANGUAGE);
+		Injector injector = null;
+		LanguageActivator activator = LanguageActivator.getInstance();
+		if (activator == null) { // Headless Eclipse
+			injector = new StatechartLanguageStandaloneSetupGenerated()
+					.createInjectorAndDoEMFRegistration();
+		}
+		else { // "Normal" Eclipse
+			injector = activator.getInjector(
+					LanguageActivator.HU_BME_MIT_GAMMA_STATECHART_LANGUAGE_STATECHARTLANGUAGE);
+		}
 		GammaLanguageSerializer serializer = injector.getInstance(GammaLanguageSerializer.class);
 		serializer.save(rootElem, URI.decode(parentFolder + File.separator + fileName));
 	}
