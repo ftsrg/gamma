6b6b1a43177065122d8b8247c3df4d4e705037d7
grbeni <grbeni@gmail.com>
2025-03-16 20:02:28 +0100

    Fix low-level any trigger trans bug

--- plugins/headless/hu.bme.mit.gamma.headless.api/src/hu/bme/mit/gamma/api/headless/WorkspaceGenerator.java
+++ plugins/headless/hu.bme.mit.gamma.headless.api/src/hu/bme/mit/gamma/api/headless/WorkspaceGenerator.java
@@ -12,7 +12,6 @@ package hu.bme.mit.gamma.api.headless;
 
 import java.util.logging.Level;
 
-import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.equinox.app.IApplicationContext;
 
@@ -26,7 +25,7 @@ public class WorkspaceGenerator extends HeadlessApplicationCommandHandler {
 
 	public void execute() throws Exception {
 		// The workspace will be generated at the destination specified after the -data argument
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		ResourcesPlugin.getWorkspace();
 		logger.info("Workspace generated successfully");
 	}
 }
